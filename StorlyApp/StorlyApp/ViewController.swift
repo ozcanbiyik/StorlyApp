840146bdca4db185463f3ea8921ba49298577778
Özcan BIYIK <ozcanbiyik@outlook.com>
2022-05-26 02:48:23 +0300

    create-app
    first commit

---
+++ StorlyApp/StorlyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StorlyApp
+//
+//  Created by Ozcan Biyik on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
